f8801901b90de683c5ee0af24b49e180b5dae98b
aaronlyy <aaronlevican@gmail.com>
2022-04-18 17:32:08 +0200

    added engines and add_keyword

---
+++ ventus/engine.py
@@ -0,0 +1,7 @@
+# engine.py
+
+class Engine:
+    GOOGLECOM = "https://google.com/search?q="
+    GOOGLEDE = "https://google.de/search?q="
+    GOOGLEFR = "https://google.fr/search?q="
+    GOOGLEES = "https://google.es/search?q="
--- ventus/query.py
+++ ventus/query.py
@@ -1,32 +1,44 @@
 # query.py
 
-from filter import Filter
-
 class Query:
-    """Build google dork query
+    """Build dork query
     """
     def __init__(self) -> None:
         self._query = ""
         self._query_size = 0
 
-    def add_filter(self, filter: str, keyword: str = "") -> None:
+    def add_filter(self, filter: str, keyword: str = "", hide=False) -> None:
         """Add a filter to the query
 
         Args:
             filter (str | Filter): custom filter or Filter.member
             keyword (str | int): Keyword for given filter
+            hide (bool): hide matches. Defaults to False.
         """
         if self._query_size > 0:
             self._query += " "
-        
+        if hide:
+            self._query += "-"
+
         self._query += f"{filter}{keyword}"
         self._query_size += 1
+
+    def add_keyword(self, keyword: str, hide=False) -> None:
+        """Add a single keyword without a filter
+
+        Args:
+            keyword (str)
+            hide (bool, optional): hide matches. Defaults to False.
+        """
+        if self._query_size > 0:
+            self._query += " "
+        if hide:
+            self._query += "-"
+        
+        self._query += keyword
+        self._query_size += 1
+        
 
     @property
     def query(self) -> str:
         return self._query
-
-q = Query()
-q.add_filter(Filter.INTITLE, "Banane")
-q.add_filter(Filter.BEFORE, "27.05.2013")
-print(q.query)
