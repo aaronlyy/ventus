7956a3fd634ce557d8e4b7e94127988606d04701
aaronlyy <aaronlevican@gmail.com>
2022-04-18 13:44:23 +0200

    added filters

---
+++ ventus/ventus.py
@@ -0,0 +1,14 @@
+# ventus.py
+from filter import Filter
+
+# build queries
+# get results
+
+
+class Search:
+    def __init__(self):
+        self._query = None
+        self._base_url = "https://google.com/search?q="
+
+    def add_filter(self):
+        pass
