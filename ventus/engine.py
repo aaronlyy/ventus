3805f851fc4b71b470bb2b9345a528d9d220eba7
aaronlyy <aaronlevican@gmail.com>
2023-06-04 21:31:52 +0200

    removed files

--- ventus/engine.py
+++
@@ -1,6 +0,0 @@
-class Engine:
-    """Searchengine URL's"""
-    GOOGLECOM = "https://google.com/search"
-    GOOGLEDE = "https://google.de/search"
-    GOOGLEFR = "https://google.fr/search"
-    GOOGLEES = "https://google.es/search"
